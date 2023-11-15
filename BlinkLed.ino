8fb04891859edd30ef911393ecd342fc371ee813
Harrison <80797691+Halikonu@users.noreply.github.com>
2023-11-14 21:03:36 -0500

    final Change

--- BlinkLed.ino
+++ BlinkLed.ino
@@ -6,11 +6,23 @@ int buttonPin = 7;
 void setup() {
   pinMode(ledPin,OUTPUT);
   pinMode(ledPin2,OUTPUT);
-  pinMode(buttonPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ if (digitalRead(buttonPin)==HIGH){
+  checkButton();
+ }
 
+}
+
+
+void checkButton(){
+digitalWrite(ledPin,HIGH);
+ digitalWrite(ledPin2,HIGH);
+ delay(500);
+ digitalWrite(ledPin,LOW);
+ digitalWrite(ledPin2,LOW);
+ delay(500);
 }
